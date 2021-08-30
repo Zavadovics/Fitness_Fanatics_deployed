91685a614944b28ba0dd6e9dfacf8d351aa3063d
Zavadovics <zavadovics@gmail.com>
2021-08-30 15:55:55 +0200

    project ready for deployment

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import dotenv from 'dotenv';
 import db from './db.js';
 import logger from './logger.js';
 import app from './app.js';
+import path from 'path'; /* deploy */
 
 dotenv.config();
 db();
 
 const PORT = process.env.PORT || 6000;
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+}); /* deploy */
 
 app.listen(PORT, () => {
   logger.info(`app is listening on port: ${PORT}`);
