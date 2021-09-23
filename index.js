0196f29b1a0dd27c074bdee536fc2085a9d12cc6
Zavadovics <zavadovics@gmail.com>
2021-09-23 13:54:46 +0200

    changes to package.json

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ db();
 
 const PORT = process.env.PORT || 6000;
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, './client', './build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, './client', './build', 'index.html'));
 });
 
 app.listen(PORT, () => {
